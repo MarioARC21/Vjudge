8783a1ecd202a375a8d9a4432661bb641091fc17
MarioARC21 <calderonriveramarioalberto1iv9@gmail.com>
2024-02-08 03:04:30 -0600

    first commit

---
+++ solve-me-first.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+	public class SolveMeFirst{
+
+		public static void main (String[] args){
+
+		Scanner lectura = new Scanner (System.in);
+
+			int a = lectura.nextInt();
+
+			int b = lectura.nextInt();
+
+		System.out.println( a + b );
+
+
+	}
+}
