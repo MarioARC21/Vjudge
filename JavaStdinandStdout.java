f5a5b00cd209b5772e5f8d077245280615cf7cc0
MarioARC21 <calderonriveramarioalberto1iv9@gmail.com>
2024-02-08 18:43:00 -0600

    third commit

---
+++ JavaStdinandStdout.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class stdinsout{
+
+	public static void main (String[] args){
+
+		Scanner scanner = new Scanner(System.in);
+
+		int myInt1 = scanner.nextInt();
+		int _myInt2 = scanner.nextInt();
+		int $myInt3 = scanner.nextInt();
+
+		System.out.println(myInt1);
+		System.out.println(_myInt2);
+		System.out.println($myInt3);
+		
+	}
+}
