d0137ab8513542262d7e5c437402dbbd0770970e
MarioARC21 <calderonriveramarioalberto1iv9@gmail.com>
2024-02-09 00:22:26 -0600

    4th commit

---
+++ JavaLoops.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class JavaLoops{
+
+    public static void main(String[] args) {
+        
+            Scanner scan = new Scanner(System.in);
+
+            int a = scan.nextInt();
+
+            for (int i = 1; i <= 10; i++) {
+            int b = a * i;
+            System.out.println(a + " x " + i + " = " + b);
+            
+        }
+    }
+
+}
