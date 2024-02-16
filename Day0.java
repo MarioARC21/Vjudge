0fd3036db27c6567754afad770035de7968d0702
MarioARC21 <marioacr21jk@gmail.com>
2024-02-15 21:11:00 -0600

    11th commit

---
+++ Day0.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class HelloWorld
+{
+	public static void main(String[] args) {
+	    
+	    Scanner lectura = new Scanner (System.in);
+
+		String a = lectura.nextLine();
+
+		System.out.println("Hello, World.");
+        System.out.println(a);
+	}
+}
